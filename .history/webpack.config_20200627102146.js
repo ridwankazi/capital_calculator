f592c00b1aec435ed16cd8310a5b9609ad350f6e
Ridwan Kazi <kaziprime@gmail.com>
2020-06-27 12:26:25 -0400

    removed node_modules and .history folders to remove them from git hub

--- .history/webpack.config_20200627102146.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist')
-    }
-}
