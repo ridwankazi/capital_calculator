ea72eac6e8b49ad03075ebe15cc26eaea07b57e5
Ridwan Kazi <kaziprime@gmail.com>
2020-06-27 17:09:11 -0400

    added flexbox less file and reorganized less files

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
         return (
             <div>
                 <h1>
-                    My React App
+                    Capital Calculator
                 </h1>
             </div>
         )
