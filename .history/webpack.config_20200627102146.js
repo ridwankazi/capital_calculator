54b6596e9f460171249b40042c6a5d49792f5d8e
Ridwan Kazi <kaziprime@gmail.com>
2020-06-27 12:20:54 -0400

    Initial commit, added boiler plate react app with webpack-dev-server

---
+++ .history/webpack.config_20200627102146.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist')
+    }
+}
