f7eea02cb8e8af0934274363bb2fb5963f92dcb4
Ridwan Kazi <kaziprime@gmail.com>
2020-06-27 18:06:02 -0400

    added boiler plate calculator component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import './App.less';
-
+import Calculator from '@capital/components/Calculator';
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>
-                    Capital Calculator
-                </h1>
+                <Calculator />
             </div>
         )
     }
---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,11 @@
+import React, {useState, useMemo} from 'react';
+
+const Calculator = ({}) => {
+    return(
+        <div style={{width: '10rem', height: '10rem'}}>
+            Calculator Box
+        </div>
+    )
+}
+
+export default Calculator;
