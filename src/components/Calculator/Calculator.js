3b8908b61aad78358388cd342e1ed9e91ecfe022
Ridwan Kazi <kaziprime@gmail.com>
2020-06-28 16:18:04 -0400

    added first iteration of Display styling and added state vars to Calculator component

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,49 +1,96 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Calculator.less'
 import ButtonsRow from '@capital/components/ButtonsRow';
 import Display from '@capital/components/Display';
 
-const Calculator = ({}) => {
-    let buttonConfigRowsList = [
-        [
-            {
-                value: 1
-            },
-            {
-                value: 2
-            },
-            {
-                value: 3
-            },
-            {
-                value: 4
-            },
-        ],
-        [
-            {
-                value: 1
-            },
-            {
-                value: 2
-            },
-            {
-                value: 3
-            },
-            {
-                value: 4
-            },
-        ]
+const buttonConfigRowsList = [
+    [
+        {
+            value: "C"
+        },
+        {
+            value: "+/-"
+        },
+        {
+            value: "%"
+        },
+        {
+            value: "/"
+        },
+    ],
+    [
+        {
+            value: "7"
+        },
+        {
+            value: "8"
+        },
+        {
+            value: "9"
+        },
+        {
+            value: "x"
+        },
+    ],
+    [
+        {
+            value: "4"
+        },
+        {
+            value: "5"
+        },
+        {
+            value: "6"
+        },
+        {
+            value: "+"
+        },
+    ],
+    [
+        {
+            value: "1"
+        },
+        {
+            value: "2"
+        },
+        {
+            value: "3"
+        },
+        {
+            value: "+"
+        },
+    ],
+    [
+        {
+            value: "0"
+        },
+        {
+            value: "."
+        },
+        {
+            value: "="
+        },
     ]
-    
-    const processButtonClick = (clickEvent, buttonValue) => {
+]
+
+const Calculator = ({}) => {
+
+    const [runningTotal, setRunningTotal] = useState("0")
+
+    const processButtonClick = (buttonValue) => {
         console.log(buttonValue)
+        if (runningTotal === "0") {
+            setRunningTotal(buttonValue)
+        } else {
+            setRunningTotal(runningTotal + buttonValue)
+        }
     }
 
 
     return(
         <div className="Calculator-Box flex-column">
             <div className="Display">
-                <Display runningTotal={100}/>
+                <Display runningTotal={runningTotal}/>
             </div>
             <div className="Buttons flex-column">
                 {buttonConfigRowsList.map( buttonConfigList => (
