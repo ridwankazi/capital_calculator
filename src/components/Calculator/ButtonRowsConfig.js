e33d139992a3d2355f6eae7c900189b0b3e5a195
Ridwan Kazi <kaziprime@gmail.com>
2020-06-28 22:01:16 -0400

    created new ButtonRowsConfig file to hold button rows config object

---
+++ src/components/Calculator/ButtonRowsConfig.js
@@ -0,0 +1,94 @@
+import {add, subtract, multiply, divide} from '@capital/operation_support/OperationSupport'
+import {
+    BUTTON_VALUE_ZERO,
+    BUTTON_VALUE_ONE,
+    BUTTON_VALUE_TWO,
+    BUTTON_VALUE_THREE,
+    BUTTON_VALUE_FOUR,
+    BUTTON_VALUE_FIVE,
+    BUTTON_VALUE_SIX,
+    BUTTON_VALUE_SEVEN,
+    BUTTON_VALUE_EIGHT,
+    BUTTON_VALUE_NINE,
+    BUTTON_VALUE_CLEAR,
+    BUTTON_VALUE_EQUAL,
+    BUTTON_VALUE_PERCENT,
+    BUTTON_VALUE_POSITIVE_NEGATIVE,
+    BUTTON_VALUE_ADD,
+    BUTTON_VALUE_SUBTRACT,
+    BUTTON_VALUE_DIVIDE,
+    BUTTON_VALUE_MULTIPLY,
+} from '@capital/Constants'
+
+const ButtonRowsConfig = [
+    [
+        {
+            value: BUTTON_VALUE_CLEAR
+        },
+        {
+            value: BUTTON_VALUE_POSITIVE_NEGATIVE
+        },
+        {
+            value: BUTTON_VALUE_PERCENT
+        },
+        {
+            value: BUTTON_VALUE_DIVIDE,
+            operationFunction: divide,
+        },
+    ],
+    [
+        {
+            value: BUTTON_VALUE_SEVEN
+        },
+        {
+            value: BUTTON_VALUE_EIGHT
+        },
+        {
+            value: BUTTON_VALUE_NINE
+        },
+        {
+            value: BUTTON_VALUE_MULTIPLY,
+            operationFunction: multiply,
+        },
+    ],
+    [
+        {
+            value: BUTTON_VALUE_FOUR
+        },
+        {
+            value: BUTTON_VALUE_FIVE
+        },
+        {
+            value: BUTTON_VALUE_SIX
+        },
+        {
+            value: BUTTON_VALUE_SUBTRACT,
+            operationFunction: subtract,
+        },
+    ],
+    [
+        {
+            value: BUTTON_VALUE_ONE
+        },
+        {
+            value: BUTTON_VALUE_TWO
+        },
+        {
+            value: BUTTON_VALUE_THREE
+        },
+        {
+            value: BUTTON_VALUE_ADD,
+            operationFunction: add,
+        },
+    ],
+    [
+        {
+            value: BUTTON_VALUE_ZERO
+        },
+        {
+            value: BUTTON_VALUE_EQUAL
+        },
+    ]
+]
+
+export default ButtonRowsConfig;
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,98 +2,12 @@ import React, {useState} from 'react';
 import './Calculator.less'
 import ButtonsRow from '@capital/components/ButtonsRow';
 import Display from '@capital/components/Display';
-import {add, subtract, multiply, divide} from '@capital/operation_support/OperationSupport'
+import ButtonRowsConfig from './ButtonRowsConfig'
 import {
     BUTTON_VALUE_ZERO,
-    BUTTON_VALUE_ONE,
-    BUTTON_VALUE_TWO,
-    BUTTON_VALUE_THREE,
-    BUTTON_VALUE_FOUR,
-    BUTTON_VALUE_FIVE,
-    BUTTON_VALUE_SIX,
-    BUTTON_VALUE_SEVEN,
-    BUTTON_VALUE_EIGHT,
-    BUTTON_VALUE_NINE,
     BUTTON_VALUE_CLEAR,
     BUTTON_VALUE_EQUAL,
-    BUTTON_VALUE_PERCENT,
-    BUTTON_VALUE_POSITIVE_NEGATIVE,
-    BUTTON_VALUE_ADD,
-    BUTTON_VALUE_SUBTRACT,
-    BUTTON_VALUE_DIVIDE,
-    BUTTON_VALUE_MULTIPLY,
 } from '@capital/Constants'
-
-const buttonConfigRowsList = [
-    [
-        {
-            value: BUTTON_VALUE_CLEAR
-        },
-        {
-            value: BUTTON_VALUE_POSITIVE_NEGATIVE
-        },
-        {
-            value: BUTTON_VALUE_PERCENT
-        },
-        {
-            value: BUTTON_VALUE_DIVIDE,
-            operationFunction: divide,
-        },
-    ],
-    [
-        {
-            value: BUTTON_VALUE_SEVEN
-        },
-        {
-            value: BUTTON_VALUE_EIGHT
-        },
-        {
-            value: BUTTON_VALUE_NINE
-        },
-        {
-            value: BUTTON_VALUE_MULTIPLY,
-            operationFunction: multiply,
-        },
-    ],
-    [
-        {
-            value: BUTTON_VALUE_FOUR
-        },
-        {
-            value: BUTTON_VALUE_FIVE
-        },
-        {
-            value: BUTTON_VALUE_SIX
-        },
-        {
-            value: BUTTON_VALUE_SUBTRACT,
-            operationFunction: subtract,
-        },
-    ],
-    [
-        {
-            value: BUTTON_VALUE_ONE
-        },
-        {
-            value: BUTTON_VALUE_TWO
-        },
-        {
-            value: BUTTON_VALUE_THREE
-        },
-        {
-            value: BUTTON_VALUE_ADD,
-            operationFunction: add,
-        },
-    ],
-    [
-        {
-            value: BUTTON_VALUE_ZERO
-        },
-        {
-            value: BUTTON_VALUE_EQUAL
-        },
-    ]
-]
 
 const Calculator = ({}) => {
 
@@ -167,7 +81,7 @@ const Calculator = ({}) => {
                 <Display displayValue={displayValue}/>
             </div>
             <div className="Buttons flex-column">
-                {buttonConfigRowsList.map( (buttonConfigList, index) => (
+                {ButtonRowsConfig.map( (buttonConfigList, index) => (
                     <ButtonsRow key={index} buttonConfigList={buttonConfigList} buttonCallBack={processButtonClick} />
                 ))}
             </div>
